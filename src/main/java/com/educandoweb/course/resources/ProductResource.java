0a0659a6d0bc04e3aeace9d207660627ed6d2cd0
WMBarbosa <wesleymario01@gmail.com>
2024-06-20 16:02:45 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/course/resources/ProductResource.java
+++ src/main/java/com/educandoweb/course/resources/ProductResource.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/product")
+@RequestMapping(value = "/products")
 public class ProductResource {
 
 
