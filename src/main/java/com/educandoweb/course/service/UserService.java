3daa1cd3983b2af3be95d6c97e2be9da60ceb890
WMBarbosa <wesleymario01@gmail.com>
2024-06-24 13:49:57 -0300

    User delete, User update

--- src/main/java/com/educandoweb/course/service/UserService.java
+++ src/main/java/com/educandoweb/course/service/UserService.java
@@ -26,4 +26,21 @@ public class UserService {
     public User insert(User user) {
         return repository.save(user);
     }
+
+    public void delete(long id) {
+        repository.deleteById(id);
+    }
+
+    public User update(Long id, User user) {
+        User entity = repository.getReferenceById(id);
+        updateData(entity, user);
+        return repository.save(entity);
+    }
+
+    private void updateData(User entity, User user) {
+        entity.setName(user.getName());
+        entity.setEmail(user.getEmail());
+        entity.setPhone(user.getPhone());
+    }
+
 }
